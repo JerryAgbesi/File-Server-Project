3d4b4e3600c1028414d5c691c851e7670c8e10ee
JerryAgbesi <jerryeagbesi@gmail.com>
2023-05-06 10:47:23 +0000

    complete logic for authentication

--- fileServer/fileServer/settings.py
+++ fileServer/fileServer/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     'django.contrib.admin',
+    'crispy_forms',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_bootstrap4',
     'fileapp',
     'users',
 ]
@@ -119,7 +122,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
--- fileServer/users/views.py
+++ fileServer/users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserSignUpForm
 
-# Create your views here.
+ 
+def signup(request):
+    
+
+    if request.method == "POST":
+        form = UserSignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            return redirect('login')
+            
+    else:
+        form = UserSignUpForm( )
+    return render(request,"users/signup.html",{'form':form})
+    
+ 
+  

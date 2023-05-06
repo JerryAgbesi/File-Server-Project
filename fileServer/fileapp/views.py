8eb4f529f6dddcad8cba1e7ffcf506983e3e90f7
JerryAgbesi <jerryeagbesi@gmail.com>
2023-05-06 12:47:28 +0000

    Add bootstrap to base.html

--- fileServer/fileapp/views.py
+++ fileServer/fileapp/views.py
@@ -2,7 +2,25 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import File
 
-# Create your views here.
+files = [
+    {
+        'title': "Wedding invite",
+        'description': 'wedding invite for Eric and Vera',
+        'date_uploaded': 'May 7, 2022',
+        'number_of_emails': '0',
+        'number_of_downloads': '0'
+
+    },
+    {
+        'title': "Funeral Brochure",
+        'description': 'Funeral brochure of ..',
+        'date_uploaded': 'May 19, 2022',
+        'number_of_emails': '0',
+        'number_of_downloads': '0'
+ 
+    }
+]
 
 class HomeView(TemplateView):
     template_name = "fileapp/home.html"
+    extra_context = {"files":files}

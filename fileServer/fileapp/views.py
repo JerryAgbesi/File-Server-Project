5cc56685be6c93f2781001386913743fbe2aced3
JerryAgbesi <jerryeagbesi@gmail.com>
2023-05-09 19:32:54 +0000

    Add count download feature

--- fileServer/fileapp/views.py
+++ fileServer/fileapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
 from .models import File
 
 #Ensure users are authenticated before accessing pages.
@@ -8,3 +10,18 @@ from .models import File
 class HomeView(LoginRequiredMixin,TemplateView):
     template_name = "fileapp/home.html"
     extra_context = {"files":File.objects.all()}
+    
+
+def file_download(request,file_id):
+    file = get_object_or_404(File,pk=file_id)
+    file.number_of_downloads += 1
+    file.save()
+    response = HttpResponse(file.file,content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="{file.file.name}"'
+    
+    return response
+
+#function to increase number of downloads 
+# def increment_downloads(request,file_id):
+#     file = get_object_or_404(File,pk=file_id)
+#     file.number_of_downloads += 1

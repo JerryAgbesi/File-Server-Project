35654c723a7293085998522e51b2c2c1abe146c6
JerryAgbesi <jerryeagbesi@gmail.com>
2023-05-14 20:35:50 +0000

    Remove default database string

--- fileServer/fileServer/settings.py
+++ fileServer/fileServer/settings.py
@@ -97,10 +97,12 @@ WSGI_APPLICATION = 'fileServer.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(env= os.environ.get('DATABASE_URL') , default='postgresql://postgres:postgres@localhost:5432/mysite' ,conn_max_age=600 )
+    'default': dj_database_url.config(env= os.environ.get('DATABASE_URL')  ,conn_max_age=600 )
+
     
 }
 
+# default='postgresql://postgres:postgres@localhost:5432/mysite'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators

63dd96c9add4fb3ea76964e309a8a3a9764fcab2
JerryAgbesi <jerryeagbesi@gmail.com>
2023-05-15 23:19:59 +0000

    comment out email sending function

--- fileServer/fileapp/views.py
+++ fileServer/fileapp/views.py
@@ -19,6 +19,7 @@ class HomeView(LoginRequiredMixin,TemplateView):
 
 def file_download(request,file_id):
     file = get_object_or_404(File,pk=file_id)
+    #increment the number of downloads 
     file.number_of_downloads += 1
     file.save()
     response = HttpResponse(file.file,content_type='application/pdf')
@@ -88,10 +89,10 @@ def send_mail(request,file_id):
                     to = [form.cleaned_data['to']])
                 
                 #attach file content to email being sent
-                email.attach(file_obj.title,file_content,'application/octet-stream')
+                # email.attach(file_obj.title,file_content,'application/octet-stream')
 
                 
-                email.send()
+                # email.send()
 
                 #Increase the count emails sent for the file
                 file_obj.number_of_emails += 1

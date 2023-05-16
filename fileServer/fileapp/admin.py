d1f0439cbbbb70f5aa50736cdefe9fcd93e31203
JerryAgbesi <jerryeagbesi@gmail.com>
2023-05-16 17:51:54 +0000

    change location of user registration

--- fileServer/fileapp/admin.py
+++ fileServer/fileapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import File
+from django.contrib.auth.models import User
 
 # Register File model to show files on the admin page
 admin.site.register(File)
+admin.site.register(User)

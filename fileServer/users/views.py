bce4277a01e60a3699a50d35f9b30002a4b1e781
JerryAgbesi <jerryeagbesi@gmail.com>
2023-05-16 17:08:18 +0000

    change username field on login form to email

--- fileServer/users/views.py
+++ fileServer/users/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate
 from django.contrib.auth.forms import UserCreationForm
+from users.forms import EmailAuthenticationForm
+from django.contrib.auth.views import LoginView
 from .forms import UserSignUpForm
+
 
  
 def signup(request):
@@ -18,5 +21,6 @@ def signup(request):
         form = UserSignUpForm( )
     return render(request,"users/signup.html",{'form':form})
     
- 
+class CustomLoginView(LoginView):
+    authentication_form = EmailAuthenticationForm
   
